a1551f2cdae1e7c6c3d86f1da1bf1af146eef014
okto prima <octoprima93@gmail.com>
2020-04-29 03:23:09 +0700

    Update config

--- generate-model.sh
+++ generate-model.sh
@@ -4,6 +4,6 @@ echo "::: starting :::"
 cd domain/core
 
 echo "::: starting generate :::"
-gen --connstr "root:empalgoreng@tcp(127.0.0.1:3306)/mkproject?&parseTime=True" --database mkproject --json
+gen --connstr "root:empalgoreng@tcp(127.0.0.1:3306)/e-wallet?&parseTime=True" --database e-wallet --json
 
 echo "::: finish :::"
